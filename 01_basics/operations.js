7a9de2cc54d0bf52efa69d33ead4960ae490b385
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-03-21 14:48:27 +0000

    container config done

---
+++ 01_basics/operations.js
@@ -0,0 +1,13 @@
+let val=3;
+let negval=-val;
+console.log(negval)
+console.log(2**3) //2^3
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
