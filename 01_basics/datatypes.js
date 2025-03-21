080dcf2984ae0a587760a59d1ede18a8da3331e8
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-03-21 09:14:34 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+// treat all js code as newer version
+//alert("3+3")
+//we are using node js not browser
+// code readability should be high
+let name ="muntaha"
+let age=18
+let isLoggedIn=false
+
+//number
+//bigint
+//string 
+//boolean
+//null => standlone value
+//undefined=>
+//symbol=>unoque
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
