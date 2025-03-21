cfbf640777496de772a495ee4ea719adaca9de3d
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-03-21 09:15:23 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="tahmin@gmail.com"
+var accountPassword="12345"
+accountCity="Chittagong"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+//accountId=2
+
+accountEmail="muntaha@gmail.com"
+accountPassword="2222"
+accountCity="Dhaka"
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+console.table([accountId,accountEmail,accountPassword,accountCity])
